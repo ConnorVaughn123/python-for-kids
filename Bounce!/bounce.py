b781ef9971b080f10b01c124847086b2eafdcf42
ConnorVaughn123 <67984369+ConnorVaughn123@users.noreply.github.com>
2020-08-05 16:59:12 -0700

    bounce game changed

--- Bounce!/bounce.py
+++ Bounce!/bounce.py
@@ -7,12 +7,20 @@ class Ball:
         self.canvas = canvas
         self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
         self.canvas.move(self.id, 245, 100)
+        self.x = 0
+        self.y = -1
+        self.canvas_height = self.canvas.winfo_height()
 
     def draw(self):
-        pass
+        self.canvas.move(self.id, self.x, self.y)
+        pos = self.canvas.coords(self.id)
+        if pos[1] <= 0:
+            self.y = 1
+        if pos[3] >= self.canvas_height:
+            self.y = -1
 
 tk = Tk()
-tk.title('Game')
+tk.title('Bounce Game')
 tk.resizable(0, 0)
 tk.wm_attributes('-topmost', 1)
 canvas = Canvas(tk, width=500, height=500, bd=0, highlightthickness=0)
@@ -21,6 +29,7 @@ tk.update()
 ball = Ball(canvas, 'red')
 
 while 1:
+    ball.draw()
     tk.update_idletasks()
     tk.update()
     time.sleep(0.01)
