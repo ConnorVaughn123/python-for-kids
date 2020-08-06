f5442e4527c4d2326f32fe30a2095784be3ea4ea
ConnorVaughn123 <67984369+ConnorVaughn123@users.noreply.github.com>
2020-08-06 15:29:27 -0700

    bounce game edited

--- Bounce!/bounce.py
+++ Bounce!/bounce.py
@@ -7,9 +7,12 @@ class Ball:
         self.canvas = canvas
         self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
         self.canvas.move(self.id, 245, 100)
-        self.x = 0
-        self.y = -1
+        starts = [-3, -2, -1, 1, 2, 3]
+        random.shuffle(starts)
+        self.x = starts[0]
+        self.y = -3
         self.canvas_height = self.canvas.winfo_height()
+        self.canvas_width = self.canvas.winfo_width()
 
     def draw(self):
         self.canvas.move(self.id, self.x, self.y)
@@ -18,6 +21,21 @@ class Ball:
             self.y = 1
         if pos[3] >= self.canvas_height:
             self.y = -1
+        if pos[0] <= 0:
+            self.x = 3
+        if pos[2] >= self.canvas_width:
+            self.x = -3
+
+class Paddle:
+     def __init__(self, canvas, color):
+        self.canvas = canvas
+        self.id = canvas.create_rectangle(0, 0, 100, 10, fill=color)
+        self.canvas.move(self.id, 200, 300)
+        self.x = 0
+        self.canvas_width = self.canvas.winfo_width()
+
+     def draw(self):
+        pass
 
 tk = Tk()
 tk.title('Bounce Game')
@@ -26,10 +44,12 @@ tk.wm_attributes('-topmost', 1)
 canvas = Canvas(tk, width=500, height=500, bd=0, highlightthickness=0)
 canvas.pack()
 tk.update()
+paddle = Paddle(canvas, 'blue')
 ball = Ball(canvas, 'red')
 
 while 1:
     ball.draw()
+    paddle.draw()
     tk.update_idletasks()
     tk.update()
     time.sleep(0.01)
