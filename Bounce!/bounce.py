4220bb77997882e17c2eefc23190b6e498c6d6ac
ConnorVaughn123 <67984369+ConnorVaughn123@users.noreply.github.com>
2020-08-06 17:15:00 -0700

    bounce game almost done

--- Bounce!/bounce.py
+++ Bounce!/bounce.py
@@ -3,8 +3,9 @@ import random
 import time
 
 class Ball:
-    def __init__(self, canvas, color):
+    def __init__(self, canvas, paddle, color):
         self.canvas = canvas
+        self.paddle = paddle
         self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
         self.canvas.move(self.id, 245, 100)
         starts = [-3, -2, -1, 1, 2, 3]
@@ -33,9 +34,22 @@ class Paddle:
         self.canvas.move(self.id, 200, 300)
         self.x = 0
         self.canvas_width = self.canvas.winfo_width()
-
+        self.canvas.bind_all('<KeyPress-Left>', self.turn_left)
+        self.canvas.bind_all('<KeyPress-Right>', self.turn_right)
+        
      def draw(self):
-        pass
+         self.canvas.move(self.id, self.x, 0)
+         pos = self.canvas.coords(self.id)
+         if pos[0] <= 0:
+            self.x = 0
+         elif pos[2] >= self.canvas_width:
+            self.x = 0
+
+     def turn_left(self, evt):
+            self.x = -2
+
+     def turn_right(self, evt):
+            self.x = 2
 
 tk = Tk()
 tk.title('Bounce Game')
