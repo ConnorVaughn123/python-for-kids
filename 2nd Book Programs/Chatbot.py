c1d948930dfab74e106addcd4211a1ba694d911a
ConnorVaughn123 <67984369+ConnorVaughn123@users.noreply.github.com>
2020-08-12 17:32:16 -0700

    Made folders for files and edited chatbot program

---
+++ 2nd Book Programs/Chatbot.py
@@ -0,0 +1,46 @@
+# This is a chatbot project
+# chatbot introduction here
+print('Hello. My name is XycoBot 1000. I am a chatbot')
+print('I like video games and food. I also like to talk about animals.')
+name = input('By the way, what is your name?: ')
+print('Cool name! Nice to meet you,', name)
+# get year info
+year = input('I am not very good at dates. What is the year?: ')
+print('Yes, I think that is correct. Thanks!')
+# ask user to guess age
+myage = input('Can you guess my age?: ')
+print('Yes you are right. I am', myage)
+# do math to calculate when chatbot will be 100
+myage = int(myage)
+nyears = 100 - myage
+print('I will be 100 in', nyears, 'years')
+print('That will be the year', int(year) + nyears)
+# video game conversation
+fav_game = input('What is your favorite video game?: ')
+print('My favorite video game is', fav_game,'too!')
+print('That game is so cool!')
+print('I also like Mario Kart.')
+fav_character = input('Who is your favorite character?: ')
+print('Oh,', fav_character, 'is your favorite?')
+print('Mine is Pacman because I like the fruit he throws.')
+# food conversation
+fav_fruit = input('Speaking of fruit, what fruit is your favorite?: ')
+print('I like', fav_fruit, 'too, but they are not my favorite.')
+print('My favorite fruit is cherries.')
+fav_food = input('I like to eat all food, not just fruit. What is your favorite food?: ')
+print('I like', fav_food, 'too, but my favorite food is fish tacos.')
+print('They taste so good.')
+# drive to restuarant
+print('All this talk about food is making me hungry. Let\'s go to a restuarant!')
+restaurant = input('Where should we go?: ')
+print('I\'ve never been to', restaurant, 'before, but it sounds good!')
+print('Let\'s drive there in my Tesla.')
+print('We\'ll be there in 5 minutes. While we\'re driving there, I will tell you about Teslas.')
+print('Teslas are electric, which means they don\'t harm the environment.')
+print('That is why I bought this car. I love animals and don\'t want them to get hurt.')
+print('This is called a Model 3. It is the newest Tesla.')
+print('Ok, we are there.')
+
+
+
+
