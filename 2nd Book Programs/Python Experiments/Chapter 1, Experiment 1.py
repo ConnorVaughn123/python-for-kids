cc2e9d12a3fc2c7ed77ff7e4a937a3221e6d9ceb
ConnorVaughn123 <67984369+ConnorVaughn123@users.noreply.github.com>
2020-08-14 14:30:14 -0700

    Rearranged folders and did experiment

---
+++ 2nd Book Programs/Python Experiments/Chapter 1, Experiment 1.py
@@ -0,0 +1,6 @@
+object = input('Name an object in this room: ')
+food = input('What kind of food do you like?: ')
+color = input('What\'s your favorite color?: ')
+zoo_animal = input('Enter the name of a zoo animal: ')
+
+print('The', zoo_animal, 'jumped onto the', color, object, 'and flew across the city to eat', food, 'at his favorite restaurant.')
---
+++ Chapter 1, Experiment 1.py
@@ -0,0 +1,6 @@
+_object_ = input('Name an object in this room: ')
+food = input('What kind of food do you like?: ')
+color = input('What\'s your favorite color?: ')
+zoo_animal = input('Enter the name of a zoo animal: ')
+
+print('The', zoo_animal, 'jumped onto the', color, _object_, 'and flew across the city to eat', food, 'at his favorite restaurant.')
