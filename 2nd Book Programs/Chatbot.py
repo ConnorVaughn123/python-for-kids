09b9e0e649253d5bbfb82be13d16847651254b6c
ConnorVaughn123 <67984369+ConnorVaughn123@users.noreply.github.com>
2020-08-13 17:15:44 -0700

    chatbot done

--- 2nd Book Programs/Chatbot.py
+++ 2nd Book Programs/Chatbot.py
@@ -1,6 +1,6 @@
 # This is a chatbot project
 # chatbot introduction here
-print('Hello. My name is XycoBot 1000. I am a chatbot')
+print('Hello. My name is XycoBot 1000. I am a robot.')
 print('I like video games and food. I also like to talk about animals.')
 name = input('By the way, what is your name?: ')
 print('Cool name! Nice to meet you,', name)
@@ -19,27 +19,65 @@ print('That will be the year', int(year) + nyears)
 fav_game = input('What is your favorite video game?: ')
 print('My favorite video game is', fav_game,'too!')
 print('That game is so cool!')
-print('I also like Mario Kart.')
+print('I also like Super Smash Bros.')
 fav_character = input('Who is your favorite character?: ')
 print('Oh,', fav_character, 'is your favorite?')
 print('Mine is Pacman because I like the fruit he throws.')
 # food conversation
-fav_fruit = input('Speaking of fruit, what fruit is your favorite?: ')
+fav_fruit = input('Speaking of fruit, which fruit is your favorite?: ')
 print('I like', fav_fruit, 'too, but they are not my favorite.')
 print('My favorite fruit is cherries.')
 fav_food = input('I like to eat all food, not just fruit. What is your favorite food?: ')
 print('I like', fav_food, 'too, but my favorite food is fish tacos.')
 print('They taste so good.')
-# drive to restuarant
-print('All this talk about food is making me hungry. Let\'s go to a restuarant!')
+# drive to restaurant
+print('All this talk about food is making me hungry. Let\'s go to a restaurant!')
 restaurant = input('Where should we go?: ')
 print('I\'ve never been to', restaurant, 'before, but it sounds good!')
 print('Let\'s drive there in my Tesla.')
 print('We\'ll be there in 5 minutes. While we\'re driving there, I will tell you about Teslas.')
 print('Teslas are electric, which means they don\'t harm the environment.')
 print('That is why I bought this car. I love animals and don\'t want them to get hurt.')
-print('This is called a Model 3. It is the newest Tesla.')
+print('This is called a Model 3. It is one of the newest Teslas.')
 print('Ok, we are there.')
+# ordering food
+res_order = input('What will you get?: ')
+print('Oh,', res_order, 'sounds good! I\'ll get it too!')
+print('While we are waiting for our food, let\'s talk about animals!')
+fav_ani = input('What is your favorite animal?: ')
+print(fav_ani, '! I also like them, but my favorite animal is a tiger.')
+print('Look, our food is here!')
+print('It looks so good.')
+print('Ok, I\'m going to try it.')
+print('It\'s really good. Don\'t you think so?')
+print('Ok, I\'m done! That was so good! I\'ll pay for it.')
+print('Ok, let\'s go back to your house.')
+# going back to house
+feeling = input('How are you feeling today?: ')
+print('Why are you feeling', feeling, 'now?')
+reason = input('Please tell me: ')
+print('I understand. Thanks for sharing.')
+print('Ok, we\'re back at your house.')
+# watching movie
+print('I see that you have a TV.')
+print('Let\'s watch a movie!')
+movie_choice = input('What movie should we watch?: ')
+print('I\'ve never seen', movie_choice, 'but it looks like a good movie!')
+snack_choice = input('What should we have for a snack?: ')
+print('Sounds good!')
+print('That was a good movie!')
+print('That was really fun.')
+# goodbye
+print('It has been a long day.')
+print('I am tired.')
+print('We can do this again tomorrow.')
+print('Tomorrow, let\'s play', fav_game, 'and eat', fav_food, 'and fish tacos!')
+print('Goodbye', name, 'I liked chatting with you.')
+
+
+
+
+
 
 
 
